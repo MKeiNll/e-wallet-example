aa798a1c7e127df338df4cd653a2e835cb7b266e
MKeiNll <levgirich@gmail.com>
2020-10-14 00:10:16 +0300

    fix post request authentication

--- api/src/main/java/com/example/wallet/configuration/SecurityConfiguration.java
+++ api/src/main/java/com/example/wallet/configuration/SecurityConfiguration.java
@@ -28,7 +28,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
